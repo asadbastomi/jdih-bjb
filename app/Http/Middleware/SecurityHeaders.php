fbb271ec99685d2baafdfbad12ed11748ac10a45
asadbastomi <asad.bastomi@gmail.com>
2026-03-14 03:55:02 +0800

    done

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -17,12 +17,12 @@ class SecurityHeaders
         $response->headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
 
         // CSP yang sudah disesuaikan agar Leaflet, Google Fonts, dan CDN pihak ketiga bisa berjalan
-        $csp = "default-src 'self'; " .
-               "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com https://cdn.jsdelivr.net https://translate.google.com https://static.elfsight.com https://static.cloudflareinsights.com; " .
-               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net https://unpkg.com; " .
-               "font-src 'self' https://fonts.gstatic.com data:; " .
-               "img-src 'self' data: https://upload.wikimedia.org https://play-lh.googleusercontent.com https://unpkg.com https://*.tile.openstreetmap.org; " .
-               "connect-src 'self' https://translate.googleapis.com;";
+         $csp = "default-src 'self'; " .
+             "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com https://cdn.jsdelivr.net https://translate.google.com https://translate.googleapis.com https://cdn.userway.org https://static.elfsight.com https://static.cloudflareinsights.com; " .
+             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net https://unpkg.com https://www.gstatic.com; " .
+             "font-src 'self' https://fonts.gstatic.com data:; " .
+             "img-src 'self' data: https://upload.wikimedia.org https://play-lh.googleusercontent.com https://unpkg.com https://*.tile.openstreetmap.org; " .
+             "connect-src 'self' https://translate.googleapis.com https://unpkg.com https://cdn.jsdelivr.net https://core.service.elfsight.com;";
                
         $response->headers->set('Content-Security-Policy', $csp);
 
